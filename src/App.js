f1058ec3d903dd1b91fc2c31609dadbecd53d898
KaterynaTrofimyuk <73081823+Katrin18@users.noreply.github.com>
2022-01-28 14:42:32 +0200

    hw-4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {useReducer} from 'react';
+import {type} from '@testing-library/user-event/dist/type';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'inc1':
+      return {...state, count1: state.count1 + 1}
+    case 'dec1':
+      return {...state, count1: state.count1 - 1}
+    case 'res1':
+      return {...state, count1: 0}
+    case 'inc2':
+      return {...state, count2: state.count2 + 1}
+    case 'dec2':
+      return {...state, count2: state.count2 - 1}
+    case 'res2':
+      return {...state, count2: 0}
+    case 'inc3':
+      return {...state, count3: state.count3 + 1}
+    case 'dec3':
+      return {...state, count3: state.count3 - 1}
+    case 'res3':
+      return {...state, count3: 0}
+  }
+}
+
+
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, {count1: 1, count2: 2, count3: 3});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div>
+          <div>{state.count1}</div>
+          <button onClick={() => dispatch({type: 'inc1'})}>INC</button>
+          <button onClick={() => dispatch({type: 'dec1'})}>DEC</button>
+          <button onClick={() => dispatch({type: 'res1'})}>RESET</button>
+        </div>
+        <div>
+          <div>{state.count2}</div>
+          <button onClick={() => dispatch({type: 'inc2'})}>INC</button>
+          <button onClick={() => dispatch({type: 'dec2'})}>DEC</button>
+          <button onClick={() => dispatch({type: 'res2'})}>RESET</button>
+        </div>
+        <div>
+          <div>{state.count3}</div>
+          <button onClick={() => dispatch({type: 'inc3'})}>INC</button>
+          <button onClick={() => dispatch({type: 'dec3'})}>DEC</button>
+          <button onClick={() => dispatch({type: 'res3'})}>RESET</button>
+        </div>
+      </div>
   );
 }
 
